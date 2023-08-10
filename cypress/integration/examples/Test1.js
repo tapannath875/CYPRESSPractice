8fe89d9f55301a6a558f9428c9d324ecf7276684
tapannath875 <tapannath875@gmail.com>
2023-08-10 08:08:36 +0530

    commit-2

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,1 +1,10 @@
-//cypress automation test
+/// <reference types="Cypress" />
+
+describe("my first test suite", function () {
+  it("my first test", function () {
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.get(".search-keyword").type("ca");
+    cy.wait(2000);
+    cy.get(".product:visible").should("have.length", 4);
+  });
+});
