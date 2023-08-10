a505d2aaf2b507435f2d90f597dc8925b8e730da
tapannath875 <tapannath875@gmail.com>
2023-08-10 09:28:12 +0530

    test2 commit

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -5,15 +5,8 @@ describe("my first test suite", function () {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get(".search-keyword").type("ca");
     cy.wait(2000);
-    cy.get(".product:visible").should("have.length", 4);
-    cy.get(".products").as("productLocator");
-    cy.get("@productLocator").find(".product").should("have.length", 4);
 
-    cy.get("@productLocator")
-      .find(".product")
-      .eq(2)
-      .contains("ADD TO CART")
-      .click();
+    cy.get(".products").as("productLocator");
 
     cy.get("@productLocator")
       .find(".product")
@@ -22,12 +15,9 @@ describe("my first test suite", function () {
         if (vegText.includes("Cashews")) {
           cy.wrap($el).find("button").click();
         }
-      }); 
-
-    cy.get(".brand").should('have.text', "GREENKART");
-
-    cy.get(".brand").then(function (e) {
-      cy.log(e.text());
-    });
+      });
+    cy.get(".cart-icon > img").click();
+    cy.contains("PROCEED TO CHECKOUT").click();
+    cy.contains("Place Order").click();
   });
 });
